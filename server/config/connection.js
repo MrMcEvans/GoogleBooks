f338d4a4d8414c5f4b7b635510785f76bd7d955b
MrMcEvans <125924206+MrMcEvans@users.noreply.github.com>
2023-10-27 12:49:50 -0500

    troubleshooting heroku

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,18 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/googlebooks', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
 
-const db = mongoose.connection;
-
-db.on('error', (error) => {
-  console.error('MongoDB connection error:', error);
-});
-
-db.once('open', () => {
-  console.log('Connected to MongoDB');
-});
-
-module.exports = db;
+module.exports = mongoose.connection;
