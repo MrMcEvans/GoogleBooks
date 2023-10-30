9bff5e614a8e27a64a0dca6a77ac7eac53697bd5
MrMcEvans <125924206+MrMcEvans@users.noreply.github.com>
2023-10-30 01:28:25 -0500

    connected clusters

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const uri = "mongodb+srv://anmcev70:<Novakin3848>@cluster0.m3mw2y2.mongodb.net/?retryWrites=true&w=majority";
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
 
