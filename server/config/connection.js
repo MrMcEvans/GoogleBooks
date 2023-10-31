21b2465dd683be5c3b67431c5895c7c21ad8b7dd
MrMcEvans <125924206+MrMcEvans@users.noreply.github.com>
2023-10-31 13:14:05 -0500

    heroku troiubleshoot

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
-const uri = "mongodb+srv://anmcev70:<Novakin3848>@cluster0.m3mw2y2.mongodb.net/?retryWrites=true&w=majority";
+const uri = "mongodb+srv://anmcev70:Novakin3848@cluster0.m3mw2y2.mongodb.net/?retryWrites=true&w=majority";
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
 
 module.exports = mongoose.connection;
+
+// rAXbZuBza2V9VSV8
